5493214cc10a0ef6d12ed4b5c3b52adef1bf9699
atgkteam-ship-it <atgkteam@gmail.com>
2026-05-09 21:06:44 +0530

    Add Firebase configuration template

---
+++ frontend/firebaseConfig.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: 'ADD_FIREBASE_API_KEY',
+  authDomain: 'falcon-ai.firebaseapp.com',
+  projectId: 'falcon-ai',
+  storageBucket: 'falcon-ai.appspot.com',
+  messagingSenderId: 'ADD_SENDER_ID',
+  appId: 'ADD_APP_ID'
+};
