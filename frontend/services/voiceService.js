3e8f67a4fce11c5f449efdd13782abaa2ffaed57
atgkteam-ship-it <atgkteam@gmail.com>
2026-05-10 00:37:48 +0530

    Add voice service structure

---
+++ frontend/services/voiceService.js
@@ -0,0 +1,7 @@
+export const startVoiceRecognition=async()=>{
+try{return true}catch(error){return false}
+}
+
+export const stopVoiceRecognition=async()=>{
+try{return true}catch(error){return false}
+}
